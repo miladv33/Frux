77df5126a6935f3fc4416e230f8eb2f70d6a20b6
Milad <milad.mosavi.developer@gmail.com>
2023-03-30 19:24:23 +0430

    complete the repository

--- app/src/main/java/com/example/frux/data/repository/PixabayImageRepository.kt
+++ app/src/main/java/com/example/frux/data/repository/PixabayImageRepository.kt
@@ -8,6 +8,7 @@ import com.example.frux.data.repository.base.IBaseRepository
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 // The repository class that fetches and maps the data from the service
@@ -16,13 +17,40 @@ import javax.inject.Inject
 class PixabayImageRepository @Inject constructor(
     private val pixabayService: PixabayService,
     private val pixarImageMapper: PixarImageMapper,
-    private val dao: HitDao
+    private val hitDao: HitDao
 ) : IBaseRepository.PixabayImageRepository() {
 
     override suspend fun searchImage(searchKey: String, imageType: String): Result<PixabayImage> {
-        val searchedImage = pixabayService.getImages(query = searchKey, imageType = imageType)
-        val pixarImage = pixarImageMapper.map(searchedImage)
-        pixarImage.getOrNull()?.hits?.let { dao.insertAll(it) }
-        return pixarImage
+        val searchedImage = getImagesFromServer(searchKey, imageType)
+        saveImagesToDatabase(searchedImage)
+        return searchedImage
     }
+
+    override suspend fun getImagesFromServer(
+        searchKey: String,
+        imageType: String
+    ): Result<PixabayImage> {
+        val images = pixabayService.getImages(query = searchKey, imageType = imageType)
+        return pixarImageMapper.map(images)
+    }
+
+    override suspend fun saveImagesToDatabase(hits: Result<PixabayImage>) {
+        hits.getOrNull()?.hits?.let { hitDao.insertAll(it) }
+    }
+
+    // Get all pixarImages from the database as a Flow object
+    override fun getPixarImages() = flow {
+        emit(hitDao.getAll())
+    }
+
+    // Get a pixarImage by its id from the database as a Flow object
+    override fun getPixarImage(id: Int)= flow {
+        emit(hitDao.get(id))
+    }
+
+    // Get all pixarImages whose tag contains a given string from the database as a Flow object
+    override fun findPixarImagesByTag(tag: String) = flow {
+        emit(hitDao.findByTag(tag))
+    }
+
 }
--- app/src/main/java/com/example/frux/data/repository/base/IBaseRepository.kt
+++ app/src/main/java/com/example/frux/data/repository/base/IBaseRepository.kt
@@ -1,10 +1,18 @@
 package com.example.frux.data.repository.base
 
+import com.example.frux.data.model.Hit
 import com.example.frux.data.model.PixabayImage
+import kotlinx.coroutines.flow.Flow
 
 interface IBaseRepository {
     abstract class PixabayImageRepository() {
         abstract suspend fun searchImage(searchKey: String, imageType: String): Result<PixabayImage>
+       abstract suspend fun getImagesFromServer(searchKey: String, imageType: String): Result<PixabayImage>
+
+        abstract suspend fun saveImagesToDatabase(hits: Result<PixabayImage>)
+        abstract fun getPixarImages(): Flow<List<Hit>>
+        abstract fun getPixarImage(id: Int): Flow<Hit>
+        abstract fun findPixarImagesByTag(query: String): Flow<List<Hit>>
     }
 
     abstract class PixarImageRepository {
