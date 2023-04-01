38d4cb0ac69067067225a79f01375603805baf42
Milad <milad.mosavi.developer@gmail.com>
2023-04-01 20:24:45 +0430

    add icons

--- app/src/main/java/com/example/frux/ui/home/ButtonSheet.kt
+++ app/src/main/java/com/example/frux/ui/home/ButtonSheet.kt
@@ -1,95 +1,79 @@
 package com.example.frux.ui.home
 
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.frux.data.model.Hit
 
 @Composable
 fun ButtonSheet(hit: Hit) {
-// Load the large image from the URL
-    val painter = rememberAsyncImagePainter(hit.largeImageURL)
-    // Create a modifier for the image size and shape
-    val modifier = Modifier
-        .fillMaxWidth()
-        .height(300.dp)
-        .clip(RoundedCornerShape(8.dp))
+    val downloadIcon = painterResource(id = com.example.frux.R.drawable.ic__download)
+    val commentIcon = painterResource(id = com.example.frux.R.drawable.ic_comment)
 
-    // Create a box to contain the image and other elements
-    Box(modifier = modifier) {
-        // Display the image with content scale to fill the box
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         Image(
-            painter = painter,
+            painter = rememberAsyncImagePainter(model = hit.largeImageURL),
             contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = modifier,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(8.dp))
         )
-        BlackBox(
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(top = 8.dp)
-                .width(100.dp)
-                .height(50.dp)
-        ) {
-            Text(
-                text = hit.views.toString(),
-                color = Color.White,
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = hit.user,
+            style = MaterialTheme.typography.h5
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = hit.tags,
+            style = MaterialTheme.typography.subtitle1
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Icon(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = null,
+                tint = Color.Red,
+                modifier = Modifier.size(24.dp)
             )
-        }
-        // Display the user image in the top left corner
-        Box(
-            modifier = Modifier.align(Alignment.TopStart)
-        ) {
-            UserImage(hit)
-        }
-        val context = LocalContext.current
-        BlackBox(modifier = Modifier
-            .align(Alignment.BottomStart)
-            .padding(8.dp)
-            .height(50.dp)
-            .clickable {
-                // Open the preview URL in a browser
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(hit.largeImageURL))
-                context.startActivity(intent)
-            }
-        ) {
-            Text(
-                text = hit.previewURL,
-                color = Color.Blue,
-                maxLines = 1
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = hit.likes.toString())
+            Spacer(modifier = Modifier.width(16.dp))
+            Icon(
+                painter = downloadIcon,
+                contentDescription = null,
+                tint = Color.Blue,
+                modifier = Modifier.size(24.dp)
             )
-        }
-        // Display the preview URL as a clickable link in the bottom left corner
-        // Display the likes in the bottom right corner
-        BlackBox(
-            modifier = Modifier
-                .height(50.dp)
-                .width(80.dp)
-                .align(Alignment.BottomEnd)
-                .padding(8.dp)
-        ) {
-            Text(
-                text = hit.likes.toString(),
-                color = Color.White,
-                textAlign = TextAlign.Center
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = hit.downloads.toString())
+            Spacer(modifier = Modifier.width(16.dp))
+            Icon(
+                painter = commentIcon,
+                contentDescription = null,
+                tint = Color.Green,
+                modifier = Modifier.size(24.dp)
             )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = hit.comments.toString())
         }
     }
 }
