0930e7335215b440f628630da48d8c1242cec21b
Milad <milad.mosavi.developer@gmail.com>
2023-04-06 01:51:32 +0430

    add fade animation to loading image and list

--- app/src/main/java/com/example/frux/ui/home/Common.kt
+++ app/src/main/java/com/example/frux/ui/home/Common.kt
@@ -7,10 +7,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,8 +23,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.frux.R
+import com.example.frux.data.enum.ImageLoading
 import com.example.frux.data.model.Hit
 import com.example.frux.ui.loading.SimpleArcRotation
 import com.example.frux.ui.theme.*
@@ -101,6 +109,57 @@ fun ShowMoreDetailsDialog(onYesClicked: () -> Unit, onNoClicked: () -> Unit) {
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CustomImage(
+    imageUrl: String,
+    modifier: Modifier
+) {
+    val brokenImage = painterResource(id = R.drawable.ic_image_broken)
+    Box {
+        val imageState = remember {
+            mutableStateOf(ImageLoading.LOADING)
+        }
+        AsyncImage(
+            contentScale = ContentScale.Crop,
+            modifier = modifier,
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(700)
+                .build(),
+            contentDescription = "",
+            onLoading = {
+                imageState.value = ImageLoading.LOADING
+            },
+            onError = {
+                imageState.value = ImageLoading.ERROR
+            },
+            onSuccess = {
+                imageState.value = ImageLoading.SUCCESS
+            }
+        )
+        if (imageState.value == ImageLoading.LOADING) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(cardImageHeight)
+            ) {
+                SimpleArcRotation()
+            }
+        }
+        if (imageState.value == ImageLoading.ERROR) {
+            Image(
+                painter = brokenImage, contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(cardImageHeight)
+                    .clip(
+                        Shapes.medium
+                    )
+            )
         }
     }
 }
